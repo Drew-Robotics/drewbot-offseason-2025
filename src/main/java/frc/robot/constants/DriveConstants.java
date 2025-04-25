b7d28bb35807e6381e760512e0c4c21d4d84a7d4
Pickles888 <verysaltypickles@gmail.com>
2025-04-25 16:30:31 -0700

    drive code done theorhetically

--- src/main/java/frc/robot/constants/DriveConstants.java
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -13,19 +13,19 @@ import frc.robot.motorconfigs.encoder.ConversionFactors;
 public class DriveConstants {
     public static final class kCANIDs {
         public static final class kDriveMotorCANIDs {
-            public static final int kFrontLeft = 0;
-            public static final int kFrontRight = 0;
+            public static final int kFrontLeft = 5;
+            public static final int kFrontRight = 7;
 
-            public static final int kBackLeft = 0;
-            public static final int kBackRight = 0;
+            public static final int kBackLeft = 3;
+            public static final int kBackRight = 1;
         }
 
         public static final class kTurnMotorCANIDs {
-            public static final int kFrontLeft = 0;
-            public static final int kFrontRight = 0;
+            public static final int kFrontLeft = 6;
+            public static final int kFrontRight = 8;
             
-            public static final int kBackLeft = 0;
-            public static final int kBackRight = 0;
+            public static final int kBackLeft = 4;
+            public static final int kBackRight = 2;
         }
     }
 
@@ -34,13 +34,13 @@ public class DriveConstants {
 
         public static final class kWheelOffsetConstants {
             public static final Translation2d kFrontLeftOffset = 
-                new Translation2d(0, 0);
+                new Translation2d(kWheelBase.div(2), kTrackWidth.div(2));
             public static final Translation2d kFrontRightOffset =
-                new Translation2d(0, 0);
+                new Translation2d(kWheelBase.div(2), kTrackWidth.div(2).negate());
             public static final Translation2d kBackLeftOffset = 
-                new Translation2d(0, 0);
+                new Translation2d(kWheelBase.div(2).negate(), kTrackWidth.div(2));
             public static final Translation2d kBackRightOffset =
-                new Translation2d(0, 0);
+                new Translation2d(kWheelBase.div(2).negate(), kTrackWidth.div(2).negate());
         }
     }
 
@@ -54,11 +54,11 @@ public class DriveConstants {
             public static final int kDrivingMotorPinionTeeth = 14;
 
             public static final double kDrivingMotorFreeSpeedRps = kFreeSpeedRpm / 60.0;
-            public static final double kWheelDiameterMeters = 0;
-            public static final double kWheelCircumferenceMeters = kWheelDiameterMeters * Math.PI;
+            public static final Distance kWheelDiameter = Units.Inches.of(2.82);
+            public static final double kWheelCircumferenceMeters = kWheelDiameter.in(Units.Meters) * Math.PI;
 
             public static final double kDrivingMotorReduction = (45.0 * 22.0) / (kDrivingMotorPinionTeeth * 15.0);
-            public static final double kDriveWheelFreeSpeedRps = (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters)
+            public static final double kDriveWheelFreeSpeedRps = (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters) 
                 / kDrivingMotorReduction;
 
             public static final double kPositionConversionFactor = kDrivingMotorReduction * kWheelCircumferenceMeters;
@@ -103,5 +103,6 @@ public class DriveConstants {
     public static final double kFreeSpeedRpm = 5676;
     public static final double kDrivingVelocityFeedForward = 1.0 / kFreeSpeedRpm;
 
-
+    public static final Distance kWheelBase = Units.Inches.of(23);
+    public static final Distance kTrackWidth = Units.Inches.of(23);  
 }
