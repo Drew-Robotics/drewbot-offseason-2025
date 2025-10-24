47490534a5018e2e620c2ee943bbef7a42761ae4
Pickles888 <verysaltypickles@gmail.com>
2025-10-24 15:08:51 -0700

    new gyro system (bit janky but will have to do)

--- src/main/java/frc/robot/subsystems/drive/drivecomponents/Gyroscope.java
+++ src/main/java/frc/robot/subsystems/drive/drivecomponents/Gyroscope.java
@@ -1,28 +1,39 @@
 package frc.robot.subsystems.drive.drivecomponents;
 
 import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.units.Units;
-import edu.wpi.first.units.measure.Angle;
-import frc.robot.constants.DriveConstants;
 import frc.robot.constants.DriveConstants.kGyro;
 
 public class Gyroscope {
     private final AHRS m_gyro;
 
+    private Rotation2d m_offset = Rotation2d.kZero;
+
     public Gyroscope() {
-        m_gyro = new AHRS(kGyro.kGyroComType);
+        m_gyro = new AHRS(NavXComType.kMXP_SPI);
+    }
+
+    private Rotation2d getYawRaw() {
+        return Rotation2d.fromDegrees(m_gyro.getYaw());
+    }
+   
+    private Rotation2d normalizeAngle(Rotation2d angle) {
+        return Rotation2d.fromDegrees(angle.getDegrees() % 360);
+    }
+
+    // PUBLIC FUNCTIONS
+
+    public void resetYaw() {
+        m_offset = getYawRaw();
     }
 
     public Rotation2d getYaw() {
-        Angle angleMeasure = Units.Degrees.of(
-            m_gyro.getYaw() * (DriveConstants.kGyro.kGyroInverted ? -1.0 : 1.0)
-        );
-        return new Rotation2d(angleMeasure);    
-    }
-
-    public void resetYaw() {
-        m_gyro.zeroYaw();
+        return normalizeAngle(
+            getYawRaw()
+                .minus(kGyro.kGyroOffset)
+                .minus(m_offset)
+        ).times(kGyro.kGyroInverted ? -1 : 1);
     }
 }
